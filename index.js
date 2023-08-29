22dccbc323739bcbe4be515f1d554f9d8b8717c0
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-29 15:01:28 -0400

    Lab Completed Class Functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+  let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']   
+  return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(){
+  let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];  
+  return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare){
+    return function(fare){
+        return fare * fare
+    }
+}
+
+const fareDoubler = function(fare){
+    return fare * 2
+}
+
+const fareTripler = function(fare){
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, func){
+    if (func === returnFirstTwoDrivers){
+        return drivers.slice(0,2);
+    }  
+    else if (func === returnLastTwoDrivers){
+        return drivers.slice(2);
+    }
+}
